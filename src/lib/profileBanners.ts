5e5fdb48296278cb0ac5c6da02e58fcc8c25d165
AnotherGenZ <git@angz.dev>
2026-05-03 02:14:57 -0400

    Add additional profile banners

--- src/lib/profileBanners.ts
+++ src/lib/profileBanners.ts
@@ -26,6 +26,81 @@ export const PROFILE_BANNERS: ProfileBannerOption[] = [
     src: '/profile-banners/nc-convoy.svg',
   },
   {
+    id: 'auraxium-lattice',
+    name: 'Auraxium Lattice',
+    src: '/profile-banners/auraxium-lattice.svg',
+  },
+  {
+    id: 'bastion-hangar',
+    name: 'Bastion Hangar',
+    src: '/profile-banners/bastion-hangar.svg',
+  },
+  {
+    id: 'nc-armory-sunburst',
+    name: 'NC Armory Sunburst',
+    src: '/profile-banners/nc-armory-sunburst.svg',
+  },
+  {
+    id: 'nso-hud-cache',
+    name: 'NSO HUD Cache',
+    src: '/profile-banners/nso-hud-cache.svg',
+  },
+  {
+    id: 'tr-armory-rising',
+    name: 'TR Armory Rising',
+    src: '/profile-banners/tr-armory-rising.svg',
+  },
+  {
+    id: 'vs-blueprint-grid',
+    name: 'VS Blueprint Grid',
+    src: '/profile-banners/vs-blueprint-grid.svg',
+  },
+  {
+    id: 'nexus-champion-frame',
+    name: 'Nexus Champion Frame',
+    src: '/profile-banners/nexus-champion-frame.svg',
+  },
+  {
+    id: 'anomaly-storm',
+    name: 'Anomaly Storm',
+    src: '/profile-banners/anomaly-storm.svg',
+  },
+  {
+    id: 'containment-grid',
+    name: 'Containment Grid',
+    src: '/profile-banners/containment-grid.svg',
+  },
+  {
+    id: 'esamir-whiteout',
+    name: 'Esamir Whiteout',
+    src: '/profile-banners/esamir-whiteout.svg',
+  },
+  {
+    id: 'halloween-bastion',
+    name: 'Halloween Bastion',
+    src: '/profile-banners/halloween-bastion.svg',
+  },
+  {
+    id: 'nc-medallion',
+    name: 'NC Medallion',
+    src: '/profile-banners/nc-medallion.svg',
+  },
+  {
+    id: 'oshur-tide',
+    name: 'Oshur Tide',
+    src: '/profile-banners/oshur-tide.svg',
+  },
+  {
+    id: 'ps1-legacy',
+    name: 'PS1 Legacy',
+    src: '/profile-banners/ps1-legacy.svg',
+  },
+  {
+    id: 'shattered-warpgate',
+    name: 'Shattered Warpgate',
+    src: '/profile-banners/shattered-warpgate.svg',
+  },
+  {
     id: 'hamma-feast',
     name: 'Hamma Feast',
     src: '/profile-banners/hamma-feast.svg',
