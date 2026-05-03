65fc40183c3de8e040cf8655b64d1bc8cd425b0c
AnotherGenZ <git@angz.dev>
2026-05-02 22:05:09 -0400

    relax coinflip requirement

--- src/lib/rules.ts
+++ src/lib/rules.ts
@@ -64,7 +64,7 @@ export function getDraftReadiness(event: HammaEvent) {
     }
   }
 
-  if (!event.coinflip?.result || !event.coinflip.choiceType) {
+  if (!event.coinflip?.result || !event.coinflip.winningTeamId) {
     return {
       ready: false,
       label: 'Waiting for coinflip',
